657203f5cfc37ac6db22b10c1dd1512c86f1be2b
rachit224agarwal <agarwalrachit224@gmail.com>
2025-08-22 00:18:07 +0530

    updated db & env

--- index.js
+++ index.js
@@ -3,11 +3,18 @@ const { userRouter } = require("./routes/user");
 const { courseRouter } = require("./routes/course");
 const { adminRouter } = require("./routes/admin");
 const app = express();
+const dbUrl = process.env.DB_URL;
 
 app.use("/user",userRouter)
 app.use("/admin",adminRouter)
 app.use("/course",courseRouter)
 
 
-app.listen(3000); 
-   
+
+async function main(){
+    await mongoose.connect(dbUrl)
+    app.listen(process.env.PORT); 
+    console.log(`Listening on ${process.env.PORT}`)
+}
+
+main();
