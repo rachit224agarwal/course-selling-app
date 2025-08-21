d863105d4338fa4af30e79c7e7f0f20f0e9790ac
rachit224agarwal <agarwalrachit224@gmail.com>
2025-08-21 22:03:52 +0530

    added admin route

---
+++ routes/admin.js
@@ -0,0 +1,32 @@
+const { Router } = require("express");
+const adminRouter = Router();
+
+adminRouter.post("/signup",function(req,res){
+    res.json({
+        msg : "signup"
+    })
+})
+adminRouter.post("/signin",function(req,res){
+    res.json({
+        msg : "signin"
+    })
+})
+adminRouter.post("/course",function(req,res){
+    res.json({
+        msg : "signup"
+    })
+})
+adminRouter.put("/course",function(req,res){
+    res.json({
+        msg : "signup"
+    })
+})
+adminRouter.get("/course",function(req,res){
+    res.json({
+        msg : "signup"
+    })
+})
+
+module.exports({
+    adminRouter : adminRouter
+})
